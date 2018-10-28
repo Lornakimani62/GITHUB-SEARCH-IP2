02cfdf197206b307760fec9faa18518022289229
lorna nashipae <kimanilorna@yahoo.com>
2018-10-28 20:59:42 +0300

    This commit is to add the routing feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RoutingModule} from '../app/routing/routing.module';
 
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { RepositoriesComponent } from './repositories/repositories.component';
 import { SearchComponent } from './search/search.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
     RepositoriesComponent,
-    SearchComponent
+    SearchComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule,Routes} from '@angular/router'
 
+import { UsersComponent} from '../users/users.component';
+import {RepositoriesComponent} from '../repositories/repositories.component';
+import {NotFoundComponent} from '../not-found/not-found.component';
+
+// Defining Routes
+ const routes:Routes=[
+   {path:"users",component:UsersComponent},
+   {path:"Repositories",component:RepositoriesComponent},
+   {path:"",redirectTo:"/users",pathMatch:"full"},
+   {path:'**',component:NotFoundComponent}
+ ]
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
+  exports:[RouterModule],
   declarations: []
 })
 export class RoutingModule { }
