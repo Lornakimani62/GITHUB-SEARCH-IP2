4ba707120bb7727718783c15c5f312a3101fccd5
lorna nashipae <kimanilorna@yahoo.com>
2018-10-30 14:21:49 +0300

    This commit is add changes to the

--- src/app/date.pipe.ts
+++ src/app/date.pipe.ts
@@ -5,8 +5,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DatePipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: any): number {
+
+   
+
+    let today:Date = new Date();
+    // let dateCreated = value;
+
+    let todayWithoutTime:any = new Date(today.getFullYear(),today.getMonth());
+
+      return todayWithoutTime;
+    
+    // return days
   }
 
 }
